01ebef11e8862aba2c2e03627d2ee7062a43423c
hubdogestor <sistemas@hubdogestor.com.br>
2025-09-29 14:59:04 +0000

    fix: configuração do vite para github pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,13 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: '/amazon-lp-prep/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
